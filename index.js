b578664e1dc6e3670e07cc85d97d1868fb87c1be
juan bernabe <juanbernabey@gmail.com>
2022-12-12 20:03:18 -0300

    re-entrega

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 
+//function validarIngreso () {
+ //   let usuario = prompt( "ingrese su nombre de usuario:" )
+ //   let pass = prompt("ingrese contraseña")
+ //       if (usuario === "juan" ) {
+ //          alert( "bienvenido juan")
+ //          console.log("usuario registrado")
+  //      } else {
+  //       alert( "el usuario " + usuario + " no exsiste, debe registrarse")      
+   //     }
+  
+//}
+ //validarIngreso ()
 
-function validarIngreso () {
-    let usuario = prompt( "ingrese su nombre de usuario:" )
-    let pass = prompt("ingrese contraseña")
-        if (usuario === "juan" ) {
-           alert( "bienvenido juan")
-           console.log("usuario registrado")
-        }    
-       else {
-            alert( "el usuario " + usuario + "no puede exsiste, debe registrarse")      
-   }
-}
- validarIngreso ()
+let intentos = 1
+let validar = true 
+do {
+  let usuario = prompt("ingrese usuario:")
+      if ( usuario === "juan" && intentos <= 5){
+        alert( "bienvenido " , usuario)
+        console.log("bienvenido")
+        break 
+      }
+      else {
+        alert(" usuario "  + usuario +  " no registrado intenta otra vez ")
+       intentos++
+       if (intentos > 5) {
+        alert("ya no tienes mas intentos")
+        break
+      }
+      }
+
+}while(validar)
 
